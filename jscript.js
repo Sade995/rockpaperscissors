3b457864e40bc0e8b90baf1f6b4cf6fafb310aa2
Sade995 <155255728+Sade995@users.noreply.github.com>
2024-02-11 11:20:38 -0300

    Updated game

--- jscript.js
+++ jscript.js
@@ -1,10 +1,30 @@
-let choice = ['Rock', 'Paper', 'Scissors'];
-
-const randomChoice = choice[Math.floor(Math.random() * choice.length)];
+let choice = ['rock', 'paper', 'scissors'];
 
 function getComputerChoice(choice){
-console.log(randomChoice); 
+    return choice[Math.floor(Math.random() * choice.length)];
+
+}
+console.log(getComputerChoice(choice));
+
+function playRound(playerSelection, computerSelection){
+if (playerSelection===computerSelection){
+    return "It's a tie!";
+}
+else if (playerSelection==="rock" && computerSelection==="scissors"){
+    return "You win! Rock beats scissors.";
+}
+else if (playerSelection==="paper" && computerSelection==="rock"){
+    return "You win! Paper beats rock.";
+}
+else if (playerSelection==="scissors" && computerSelection==="paper"){
+    return "You win! Scissors beat paper.";
+}
+else {
+    return "You lose! Try again"
 }
 
-const playerSelection = document.querySelector('userchoice');
+}
+const playerSelection =prompt("Pick yours").toLowerCase();
+const computerSelection = getComputerChoice(choice)
+console.log(playRound(playerSelection,computerSelection))
 
