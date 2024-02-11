7ead0696b97c2250a08f67a1368628f46ea151cc
Sade995 <155255728+Sade995@users.noreply.github.com>
2024-02-11 00:22:58 -0300

    Defined randomChoice and function

--- jscript.js
+++ jscript.js
@@ -1,3 +1,8 @@
-function getComputerChoice(){
-    
+let choice = ['Rock', 'Paper', 'Scissors'];
+
+const randomChoice = choice[Math.floor(Math.random() * choice.length)];
+
+function getComputerChoice(choice){
+console.log(randomChoice); 
 }
+
