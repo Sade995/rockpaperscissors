a909a3d02f7ba6606db965f71fff387a67db364a
Sade995 <155255728+Sade995@users.noreply.github.com>
2024-02-19 00:21:39 -0300

    update on game script.js
    Actualizaciones hechas al juego, esta vez desde cero y sin copiar código externo.

--- jscript.js
+++ jscript.js
@@ -1,33 +1,18 @@
 let choice = ['rock', 'paper', 'scissors'];
 
 function getComputerChoice(choice){
-    return choice[Math.floor(Math.random() * choice.length)];
+  return choice[Math.floor((Math.random()*choice.length))];
 
 }
+
+const computerSelection = getComputerChoice(choice);
+const playerSelection = prompt('Choose your weapon', '').toLowerCase();
 
 function playRound(playerSelection, computerSelection){
-    console.log(playerSelection, computerSelection);
-if (playerSelection===computerSelection){
-    return "It's a tie!";
-}
-else if (playerSelection==="rock" && computerSelection==="scissors"){
-    return "You win! Rock beats scissors.";
-}
-else if (playerSelection==="paper" && computerSelection==="rock"){
-    return "You win! Paper beats rock.";
-}
-else if (playerSelection==="scissors" && computerSelection==="paper"){
-    return "You win! Scissors beat paper.";
-}
-else {
-    return "You lose! Try again"
-}
+ if (playerSelection === computerSelection){
+    return "It\'s\ a tie";
+ } else 
+    return "You lose!"; }
 
-}
-const playerSelection =prompt("Pick yours").toLowerCase();
-const computerSelection = getComputerChoice(choice);
-console.log(playRound(playerSelection,computerSelection));
-
-function playGame(){
-playRound();
-}
+     
+console.log(playRound(playerSelection, computerSelection))
